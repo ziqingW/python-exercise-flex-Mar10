613e50bb92fe323236afb262679c7af6def25877
Ziqing Wang <redtaq@hotmail.com>
2018-03-10 20:47:38 +0000

    RPG game step1

---
+++ RPG_Game/game.py
@@ -0,0 +1,12 @@
+class Hero:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+
+class Goblin:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+        
+hero = Hero(10, 5)
+goblin = Goblin(6, 2)
